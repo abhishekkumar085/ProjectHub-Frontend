f8d236e6c178578b80a13c60035ef75e771b2879
samruddhi337 <samruddhinathile7@gmail.com>
2026-05-29 13:47:15 +0530

    build chnages

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import { getDashboardApi } from "../dashboard/api/DashboardApi";
 import Highcharts from "highcharts";
 import { HighchartsReact } from "highcharts-react-official";
+import Loader from "../../components/common/Loader";
 
 interface KpiTile {
   label: string;
@@ -72,9 +73,9 @@ function Dashboard() {
     fetchDashboard();
   }, []);
 
-  // if (loading) {
-  //   return <Loader />;
-  // }
+  if (loading) {
+    return <Loader />;
+  }
 
   const graphData = dashboard?.graphData ?? [];
   const maxValue = Math.max(
